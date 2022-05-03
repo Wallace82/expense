e35ab806dcf35076a267c249fc275582e69d96c1
wallace <gomesw@gmail.com>
2022-05-03 10:48:23 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas'),
+      ),
+      body: Center(
+        child: Text('Versão inicial'),
+      ),
+    );
+  }
+}
+
